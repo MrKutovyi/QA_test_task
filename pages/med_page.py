d8ab2a747fed6ff23bc032953dcb58eb4b1074c5
MrKutovyi <mr.kutovyi@gmail.com>
2021-11-19 12:46:29 +0200

    new pages added

---
+++ pages/med_page.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+from .base_element import BaseElement
+from .base_page import BasePage
+
+
+class LoginPage(BasePage):
+
+    url = 'http://demo.hospitalrun.io/#/medication'
